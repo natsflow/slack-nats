bc5374e50d8f908d96fc5100bf6e85746731ece2
nea1 <neallidster@gmail.com>
2018-11-30 18:52:12 +0000

    adding slack auth check

--- main.go
+++ main.go
@@ -7,8 +7,16 @@ import (
 	"github.com/natsflow/slack-nats/pkg/event"
 	"github.com/natsflow/slack-nats/pkg/nats"
 	"github.com/nlopes/slack"
+	"go.uber.org/zap"
 	"os"
 )
+
+var logger *zap.SugaredLogger
+
+func init() {
+	l, _ := zap.NewProduction()
+	logger = l.Sugar()
+}
 
 func main() {
 	natsURL, ok := os.LookupEnv("NATS_URL")
@@ -19,6 +27,9 @@ func main() {
 	defer n.Close()
 
 	s := slack.New(os.Getenv("SLACK_TOKEN")).NewRTM()
+	if _, err := s.AuthTest(); err != nil {
+		logger.Fatalf("failed to authenticate with slack: %v", err)
+	}
 	go s.ManageConnection()
 
 	go chat.PostMessageHandler(n, s)
